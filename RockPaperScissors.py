1518ccb162711a076f904b5ccba651f4ed360a6d
Skaz7 <sebastian.kupis@gmail.com>
2021-11-12 10:45:44 +0100

    Trzeci commit - usunięte błędy

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -48,42 +48,51 @@ def game_screen():
         print('\n\t0 - Wyjście')
 
         computer_choice = random.choice(['PAPIER', 'KAMIEŃ', 'NOŻYCE'])
+        try:
+            user_choice = int(input('\nWybieram: '))
 
-        user_choice = int(input('\nWybieram: '))
+            if user_choice == 1 and computer_choice == 'PAPIER':
+                print_paper()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
+            elif user_choice == 1 and computer_choice == 'KAMIEŃ':
+                print_rock()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
+            elif user_choice == 1 and computer_choice == 'NOŻYCE':
+                print_scissors()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
+            elif user_choice == 2 and computer_choice == 'PAPIER':
+                print_paper()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
+            elif user_choice == 2 and computer_choice == 'KAMIEŃ':
+                print_rock()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
+            elif user_choice == 2 and computer_choice == 'NOŻYCE':
+                print_scissors()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
+            elif user_choice == 3 and computer_choice == 'PAPIER':
+                print_paper()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
+            elif user_choice == 3 and computer_choice == 'KAMIEŃ':
+                print_rock()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
+            elif user_choice == 3 and computer_choice == 'NOŻYCE':
+                print_scissors()
+                print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
+            elif user_choice == 0:
+                clear_screen()
+                print('\t\t\nDO ZOBACZENIA!\n\n')
+                quit()
+            else:
+                print('\n\tBłędny wybór, powtórz...')
+                time.sleep(2)
+                continue
 
-        if user_choice == 1 and computer_choice == 'PAPIER':
-            print_paper()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
-        elif user_choice == 1 and computer_choice == 'KAMIEŃ':
-            print_rock()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
-        elif user_choice == 1 and computer_choice == 'NOŻYCE':
-            print_scissors()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
-        elif user_choice == 2 and computer_choice == 'PAPIER':
-            print_paper()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
-        elif user_choice == 2 and computer_choice == 'KAMIEŃ':
-            print_rock()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
-        elif user_choice == 2 and computer_choice == 'NOŻYCE':
-            print_scissors()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
-        elif user_choice == 3 and computer_choice == 'PAPIER':
-            print_paper()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, WYGRAŁEŚ!')
-        elif user_choice == 3 and computer_choice == 'KAMIEŃ':
-            print_rock()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, PRZEGRAŁEŚ!')
-        elif user_choice == 3 and computer_choice == 'NOŻYCE':
-            print_scissors()
-            print(f'\n\n\t\tKomputer wybrał {computer_choice}, REMIS!')
-        elif user_choice == 0:
-            clear_screen()
-            print('\t\t\nDO ZOBACZENIA!\n\n')
-            quit()
+            print()
         
-        print()
+        except ValueError:
+            print('\n\tBłędny wybór, powtórz...')
+            time.sleep(2)
+            continue
 
         time.sleep(3)
 
